251835fe717c22ee988e6c6b9098ff34aa9d3081
msaytska <msaytska@activewizards.com>
2018-02-28 17:08:07 +0100

    Add READ.me and urls

--- students/urls.py
+++ students/urls.py
@@ -5,6 +5,5 @@ from students import views
 urlpatterns = [
     # url(r'', views.index, name="index"),
     url(r'^', views.students_list, name='home'),
-    url(r'^groups/$', views.groups_list, name='groups'),
-    url(r'^students/add/$', views.students_add, name='students_add')
+    url(r'^groups/', views.groups_list, name='groups'),
 ]
